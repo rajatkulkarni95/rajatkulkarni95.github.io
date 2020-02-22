61f4a99772ee676910551afae68777d045420969
rajatkulkarni95 <rajatkulkarni95@gmail.com>
2020-02-22 22:46:58 +0530

    breakdown of seats

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -2,13 +2,15 @@
 const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".seat");
 const seat = document.querySelector(".seat");
-const count = document.querySelector("#count");
 const total = document.querySelector("#total");
 const bronzePrice = document.querySelector("#bronze-price");
 const silverPrice = document.querySelector("#silver-price");
 const goldPrice = document.querySelector("#gold-price");
 const paraText = document.querySelector(".text");
 const movieTitle = document.getElementById("movie-title");
+const bronze_seats = document.getElementById("bronze-seats");
+const silver_seats = document.getElementById("silver-seats");
+const gold_seats = document.getElementById("gold-seats");
 
 let baseTicketPrice = Math.floor(Math.random() * (13 - 8 + 1)) + 8;
 updateTicketPrice(baseTicketPrice);
@@ -20,22 +22,49 @@ movieTitle.innerHTML = title;
 function updateSelectedCount() {
   let ticketPrice = 0;
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
-
+  let bronzeSeats = 0,
+    silverSeats = 0,
+    goldSeats = 0;
   const selectedSeatCount = selectedSeats.length;
 
   const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-  console.log(seatIndex);
   seatIndex.forEach(value => {
     if (value < 20) {
+      bronzeSeats++;
       ticketPrice += baseTicketPrice;
     } else if (value < 44) {
+      silverSeats++;
       ticketPrice += Math.round(baseTicketPrice * 1.2, 2);
     } else {
+      goldSeats++;
       ticketPrice += Math.round(baseTicketPrice * 1.5, 2);
     }
   });
 
-  count.innerHTML = selectedSeatCount;
+  if (bronzeSeats !== 0) {
+    bronze_seats.innerText = `${bronzeSeats} Bronze`;
+  } else {
+    while (bronze_seats.firstChild) {
+      bronze_seats.removeChild(bronze_seats.firstChild);
+    }
+  }
+  if (silverSeats !== 0) {
+    silver_seats.innerHTML = `${silverSeats} Silver`;
+  } else {
+    while (silver_seats.firstChild) {
+      silver_seats.removeChild(silver_seats.firstChild);
+    }
+  }
+  if (goldSeats !== 0) {
+    gold_seats.innerHTML = `${goldSeats} Gold`;
+  } else {
+    while (gold_seats.firstChild) {
+      gold_seats.removeChild(gold_seats.firstChild);
+    }
+  }
+
+  console.log(bronzeSeats);
+
   total.innerHTML = ticketPrice;
 }
 
